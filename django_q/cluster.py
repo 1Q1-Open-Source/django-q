37dbd415656b77dd96090c1f940306248c0b4484
Samuel Bishop <lucractius@me.com>
2022-11-22 15:26:42 +0800

    log function names to catch failures better

--- django_q/cluster.py
+++ django_q/cluster.py
@@ -424,7 +424,7 @@ def worker(
         timer.value = -1  # Idle
         task_count += 1
         # Get the function from the task
-        logger.info(_(f'{name} processing [{task["name"]}]'))
+        logger.info(_(f'{name} processing [{task["name"]}] {task["func"]}'))
         f = task["func"]
         # if it's not an instance try to get it from the string
         if not callable(task["func"]):
